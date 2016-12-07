26ca308cc04220bf6ea89a2957135d8dcf429690
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-12-07 20:02:07 +0100

    Setup lein project

---
+++ test/clojure_leap_year/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-leap-year.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-leap-year.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
