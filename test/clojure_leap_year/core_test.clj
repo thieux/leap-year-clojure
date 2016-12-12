454afb2523c524706a9236f564de282e37ed4fde
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-12-12 22:25:42 +0100

    Create failing test: 2000 should be atypical leap

--- test/clojure_leap_year/core_test.clj
+++ test/clojure_leap_year/core_test.clj
@@ -13,3 +13,7 @@
 (deftest test-atypical-common
   (testing "1900 should be atypical common"
     (is (= (is-leap 1900) false))))
+
+(deftest test-atypical-leap
+  (testing "2000 should be atypical leap"
+    (is (= (is-leap 2000) true))))
