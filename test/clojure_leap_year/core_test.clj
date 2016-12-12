dd9be66ece149433ef9fc36c191f39e65d5ca0d1
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-12-12 21:31:12 +0100

    Introduce rule year modulo 4 should be leap

--- test/clojure_leap_year/core_test.clj
+++ test/clojure_leap_year/core_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [clojure-leap-year.core :refer :all]))
 
-(defn is-leap [y] (if (= 1996 y) true false))
+(defn is-leap [y] (if (= 0 (mod y 4)) true false))
 
 (deftest test-leap
   (testing "1996 should be leap"
