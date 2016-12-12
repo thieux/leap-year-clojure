4a06513313c216cb2c176070426dd046f6e379e6
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-12-12 22:04:40 +0100

    Format source code

--- src/clojure_leap_year/core.clj
+++ src/clojure_leap_year/core.clj
@@ -1,4 +1,9 @@
 (ns clojure-leap-year.core
   (:gen-class))
 
-(defn is-leap [y] (if (= 1900 y) false (if (= 0 (mod y 4)) true false)))
+(defn is-leap [y] 
+  (if (= 1900 y) 
+    false
+    (if (= 0 (mod y 4)) 
+      true 
+      false)))
